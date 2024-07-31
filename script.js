240fa8a63d6a8a7d26a67ab739d85b04cd6b15fc
Selina Zhu <158758761+SelinaZhu26@users.noreply.github.com>
2024-07-31 16:35:59 +0000

    javascript examples

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 
 //console log text, expression 
+// console.log("hello world")
+// console.log(2*3)
+// console.log(2/2)
 
 // Create a variable to hold your name as a string
+// let myName = "Selina"
+myName = "S"
+console.log(myName)
 
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!” 
+// let myName = "Selina"
+// const myMessage = "Good morning, "
+
+// console.log(myMessage + myName)
 
 //console log your name variable
 
 //Re-assign the value of your name variable below its declaration and console log it again
 
 //practice math operators
+// console.log(10-5)
+// console.log(10+5)
+// console.log(10*5)
+// console.log(10/5)
+// console.log(10%5)
+// console.log(10**5)
+// console.log(2*(3+2)-5)
 
 // boolean - true or false
+// let isSunny = false
+// isSunny = true
+// console.log(!isSunny)
 
 //create null and undefined
+// let isHungry = null
+// console.log(isHungry)
+
 
 //mix data types
+
 
 //string methods
 
